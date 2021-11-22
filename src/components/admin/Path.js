e4f2e4d1065e0e128650db9bc0fe6ba91a7ad378
rajjatlayal <amanlayalupwork@gmail.com>
2021-11-22 19:38:18 +0530

    Combine changes1

--- src/components/admin/Path.js
+++ src/components/admin/Path.js
@@ -1,2 +1,2 @@
-export const Path = 'http://localhost/octivius/src/components/admin/backend/';
-//export const Path = 'https://developerssupport.com/cc/backend/';
+//export const Path = 'http://localhost/octivius/src/components/admin/backend/';
+export const Path = 'https://developerssupport.com/cc/backend/';
